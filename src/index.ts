22066ff083dce472d76aa6773cd1e26bb73581f6
kotahorii <khorii.micron@gmail.com>
2021-10-24 22:27:27 +0900

    generates types for TS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import {ApolloServer} from 'apollo-server';
+import {ApolloServer, AuthenticationError} from 'apollo-server';
 import {loadSchemaSync} from '@graphql-tools/load';
 import {GraphQLFileLoader} from '@graphql-tools/graphql-file-loader';
 import {addResolversToSchema} from '@graphql-tools/schema';
 import {join} from 'path';
+import {Resolvers} from './types/generated/graphql';
+import {Context} from './types/context';
 
 const books = [
   {
@@ -19,15 +21,34 @@ const schema = loadSchemaSync(join(__dirname, '../schema.graphql'), {
   loaders: [new GraphQLFileLoader()],
 });
 
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
-    books: () => books,
+    books: (_parent, _args, _context) => books,
   },
 };
 
 const schemaWithResolvers = addResolversToSchema({schema, resolvers});
+const getUser = (token?: string): Context['user'] => {
+  if (token === undefined) {
+    throw new AuthenticationError(
+      '認証されていないユーザーはリソースにアクセスできません'
+    );
+  }
+  return {
+    name: 'dummy name',
+    email: 'dummy@example.com',
+    token,
+  };
+};
 
-const server = new ApolloServer({schema: schemaWithResolvers});
+const server = new ApolloServer({
+  schema: schemaWithResolvers,
+  context: ({req}) =>
+    ({
+      user: getUser(req.headers.authorization),
+    } as Context),
+  debug: false,
+});
 
 server.listen().then(({url}) => {
   console.log(`Server ready at ${url}`);
